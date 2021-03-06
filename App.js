ebd2c66de9d30d60254e8e32670da9200c0c8254
Diogo Bastos <diogo.bastos@hmhco.com>
2021-03-05 20:48:45 -0500

    Introducing tabs

--- App.js
+++ App.js
@@ -1,39 +1,65 @@
-import React from 'react';
+import * as React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import {COR_DE_FUNDO} from './src/styles/styles';
-import Landpage from './src/views/LandingPage';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import LandingPage from './src/views/LandingPage';
 import {DetalhesItem} from './src/views/DetalhesItem';
 import {Pastoral} from './src/views/Pastoral';
+import {COR_DE_FUNDO, ACTIVE_GREEN, INACTIVE_GRAY} from './src/styles/styles';
 
-const Stack = createStackNavigator();
+function SettingsStackScreen() {
+  return (
+    <View>
+      <Text style={{textAlign: 'center', marginTop: 300}}>Settings Screen</Text>
+    </View>
+  );
+}
 
-const App = () => {
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomeStack.Navigator initialRouteName="Landpage">
+        <HomeStack.Screen
+          name="Landpage"
+          component={LandingPage}
+          options={{headerShown: false}}
+        />
+        <HomeStack.Screen
+          name="DetalhesItem"
+          component={DetalhesItem}
+          options={{headerShown: false}}
+        />
+        <HomeStack.Screen
+          name="Pastoral"
+          component={Pastoral}
+          options={{headerShown: false}}
+        />
+      </HomeStack.Navigator>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <Stack.Navigator initialRouteName="Landpage">
-          <Stack.Screen
-            name="Landpage"
-            component={Landpage}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="DetalhesItem"
-            component={DetalhesItem}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Pastoral"
-            component={Pastoral}
-            options={{headerShown: false}}
-          />
-        </Stack.Navigator>
-      </SafeAreaView>
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: ACTIVE_GREEN,
+          inactiveTintColor: INACTIVE_GRAY,
+        }}>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Doação" component={SettingsStackScreen} />
+        <Tab.Screen name="Agenda" component={SettingsStackScreen} />
+        <Tab.Screen name="Contato" component={SettingsStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -41,5 +67,3 @@ const styles = StyleSheet.create({
     backgroundColor: COR_DE_FUNDO,
   },
 });
-
-export default App;
--- src/views/Pastoral/index.js
+++ src/views/Pastoral/index.js
@@ -1,86 +1,20 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {
-  FONT_FAMILY_BOLD,
-  FONT_FAMILY_REGULAR,
-  LIGHTERGRAY,
-  LIGHTGRAY,
-  SIZE_MEDIUM,
-  SIZE_SMALL,
-  SIZE_X_LARGE,
-  SIZE_X_SMALL,
-  WHITE,
-} from '../../styles/styles';
+import {StyleSheet, Text, View} from 'react-native';
 
 export const Pastoral = ({route}) => {
-  const {imagem, titulo, itemDesc} = route.params;
+  const {itemDesc} = route.params;
 
   return (
-    <View style={styles.itemContainer}>
-      <View style={styles.itemContainer}>
-        <View style={styles.itemPosicao}>
-          <View style={styles.item}>
-            <View style={styles.headerPosicao}>
-              <View>
-                <Text style={styles.textInferior}>{titulo}</Text>
-              </View>
-              <Image source={imagem} style={styles.imagemItem} />
-            </View>
-            <Text style={styles.textoDescricao}>{itemDesc}</Text>
-          </View>
-        </View>
-      </View>
+    <View>
+      <Text style={styles.container}>Pastoral</Text>
+      <Text>{itemDesc}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  itemContainer: {
-    flex: 4,
-    marginTop: -60,
-  },
-  itemPosicao: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  item: {
-    backgroundColor: WHITE,
-    borderRadius: 30,
-    padding: 28,
-    width: '80%',
-    elevation: 4,
-  },
-  headerPosicao: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  textSuperior: {
-    fontFamily: FONT_FAMILY_BOLD,
-    fontSize: SIZE_MEDIUM,
-    marginBottom: 4,
-  },
-  textMedio: {
-    fontFamily: FONT_FAMILY_BOLD,
-    fontSize: SIZE_X_LARGE,
-    marginBottom: 4,
-  },
-  textInferior: {
-    fontFamily: FONT_FAMILY_BOLD,
-    fontSize: SIZE_SMALL,
-    color: LIGHTGRAY,
-    marginBottom: SIZE_X_SMALL,
-  },
-  imagemItem: {
-    width: 32,
-    height: 72,
-  },
-  textoDescricao: {
-    fontFamily: FONT_FAMILY_REGULAR,
-    fontSize: SIZE_SMALL,
-    color: LIGHTERGRAY,
+    textAlign: 'center',
+    marginTop: 300,
   },
 });
