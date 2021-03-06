694badd606862146df8cee0b04a43b9043be496c
Diogo Bastos <diogo.bastos@hmhco.com>
2021-03-06 13:01:23 -0500

    Doacao component

---
+++ src/views/Doacao/data/Doacao.js
@@ -0,0 +1,9 @@
+export const DOACAO = {
+  nomeBanco: 'CAIXA ECONOMICA FEDERAL',
+  banco: 'Banco 104',
+  agencia: 'Agencia 0996',
+  cc: 'C/C 3211-6',
+  operacao: 'Operação 003',
+  igreja: 'IGREJA PRESBITERIANA MOSAICO',
+  cnpj: 'CNPJ 22.289.452/0001-78',
+};
--- src/views/Doacao/index.js
+++ src/views/Doacao/index.js
@@ -1,24 +1,94 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import {ContainerPage} from '../../components/ContainerPage';
+import {
+  FONT_FAMILY_BOLD,
+  TITLE,
+  SIZE_XXX_LARGE,
+  FONT_FAMILY_LIGHT,
+  BEIGE,
+  ORANGE,
+  FONT_FAMILY_REGULAR,
+  WHITE,
+  IRON,
+} from '../../styles/styles';
 import {IconDoacao} from '../../assets/images/Icons';
+import {DOACAO} from './data/Doacao';
 
 export const Doacao = () => {
+  const {nomeBanco, banco, agencia, cc, operacao, igreja, cnpj} = DOACAO;
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ContainerPage imagem={IconDoacao}>
-        <Text style={styles.containerTitulo}>Doação</Text>
-      </ContainerPage>
-    </SafeAreaView>
+    <ContainerPage imagem={IconDoacao}>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Dados da nossa conta</Text>
+        <View style={styles.containerConta}>
+          <View style={styles.banco}>
+            <Text style={styles.conta}>{nomeBanco}</Text>
+          </View>
+          <View style={styles.containerItem}>
+            <Text style={styles.conta}>{banco}</Text>
+            <Text style={styles.conta}>{agencia}</Text>
+            <Text style={styles.conta}>{cc}</Text>
+            <Text style={styles.conta}>{operacao}</Text>
+          </View>
+        </View>
+        <Text style={styles.detalhesIgreja}>{igreja}</Text>
+        <Text style={styles.detalhesIgreja}>{cnpj}</Text>
+      </View>
+    </ContainerPage>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  containerTitulo: {
+  conta: {
+    color: ORANGE,
+    fontSize: 14,
+    fontFamily: FONT_FAMILY_REGULAR,
     textAlign: 'center',
-    marginTop: 300,
+  },
+  titulo: {
+    color: TITLE,
+    fontSize: 16,
+    fontFamily: FONT_FAMILY_BOLD,
+  },
+  detalhesIgreja: {
+    color: TITLE,
+    fontSize: 16,
+    fontFamily: FONT_FAMILY_LIGHT,
+    textAlign: 'center',
+  },
+  containerConta: {
+    width: 284,
+    height: 132,
+    margin: SIZE_XXX_LARGE,
+  },
+  banco: {
+    width: 284,
+    height: 38,
+    backgroundColor: WHITE,
+    borderColor: IRON,
+    borderWidth: 0.5,
+    justifyContent: 'center',
+    shadowOpacity: 0.2,
+  },
+  containerItem: {
+    width: 283,
+    height: 81,
+    backgroundColor: BEIGE,
+    justifyContent: 'center',
+    shadowOffset: {
+      width: 0.2,
+      height: 0.2,
+    },
+    shadowOpacity: 0.2,
+  },
+  imagem: {
+    height: 84,
   },
 });
