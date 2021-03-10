da96c3f1b1f2386424d03b9444e402d46539896d
Diogo Bastos <diogo.bastos@hmhco.com>
2021-03-09 19:05:52 -0500

    Contato

---
+++ src/views/Contato/data/Contato.js
@@ -0,0 +1,9 @@
+export const CONTATO = {
+  logo: require('../../../assets/images/mosaico.png'),
+  endereco: 'R. T-53, 480 St. Bueno, Goiânia - Goiás, CEP 74810-210',
+  telefone: '62-99161-6086',
+  email: 'diogovvb@gmail.com ', //lobofelipe90@gmail.com
+  LATITUDE: 37.78825,
+  LONGITUDE: -122.4324,
+  LATITUDE_DELTA: 0.0922,
+};
--- src/views/Contato/index.js
+++ src/views/Contato/index.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {ContainerPage} from '../../components/ContainerPage';
+import {ACTIVE_GREEN} from '../../styles/styles';
+import {CONTATO} from './data/Contato';
 
 export const Contato = () => {
+  const {logo, endereco, telefone} = CONTATO;
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ContainerPage>
-        <Text style={styles.containerTitulo}>Contato</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <ContainerPage titulo={'Contato'}>
+        <>
+          <Image source={logo} style={styles.imagem} resizeMode="contain" />
+          <View style={styles.containerEnderecoMapa}>
+            <View>
+              <View style={styles.containerEndereco}>
+                <Image
+                  source={require('../../assets/images/location.png')}
+                  style={styles.icons}
+                />
+                <Text style={styles.endereco}>{endereco}</Text>
+              </View>
+              <View style={styles.containerEndereco}>
+                <Image
+                  source={require('../../assets/images/telefone.png')}
+                  style={styles.icons}
+                />
+                <Text style={styles.telefone}>{telefone}</Text>
+              </View>
+            </View>
+            <View style={styles.containerMapa} />
+          </View>
+        </>
       </ContainerPage>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
   },
-  containerTitulo: {
-    textAlign: 'center',
-    marginTop: 300,
+  imagem: {
+    marginBottom: -50,
+    width: 216,
+    height: 216,
+    alignSelf: 'auto',
+  },
+  icons: {
+    width: 20,
+    height: 18,
+  },
+  containerEndereco: {
+    flexDirection: 'row',
+    width: 140,
+    height: 85,
+  },
+  containerEnderecoMapa: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    alignSelf: 'auto',
+  },
+  containerMapa: {
+    width: 134,
+    height: 235,
+    borderWidth: 1,
+  },
+  endereco: {
+    color: ACTIVE_GREEN,
+  },
+  telefone: {
+    color: ACTIVE_GREEN,
   },
 });
