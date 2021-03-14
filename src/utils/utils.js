b6946bd39f955c4a084971126ad301a29eff3ac4
Diogo Bastos <diogo.bastos@hmhco.com>
2021-03-14 16:42:06 -0400

    General fixes

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,9 @@
 export const getSize = (height) => {
   let size = 'small';
 
-  if (height > 534 && height < 668) {
+  if (height > 534 && height < 670) {
     size = 'medium';
-  } else if (height > 667 && height < 897) {
+  } else if (height > 670 && height < 897) {
     size = 'regular';
   } else if (height > 896) {
     size = 'big';
