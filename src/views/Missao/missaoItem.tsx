f67f3f40a48bd80ac046dda2dcbe46d6c71fcf43
Diogo Bastos <diogo.bastos@hmhco.com>
2021-03-24 11:01:05 -0400

    Missao

--- src/views/Missao/missaoItem.tsx
+++ src/views/Missao/missaoItem.tsx
@@ -5,52 +5,42 @@ import {
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
 
-import {FONT_AVENIR_ROMAN} from '../../styles/styles';
+import {FONT_AVENIR_ROMAN, SUBTEXT} from '../../styles/styles';
 
 interface Props {
   nome: string;
-  missao: string;
-  contato: string;
+  missao: string | undefined;
+  contato: string | undefined;
   backgroundColor: string;
-  color: string;
 }
 
-export const MissaoItem = ({
-  nome,
-  missao,
-  contato,
-  backgroundColor,
-  color,
-}: Props) => {
-  const styles = getStyles(backgroundColor, color);
+export const MissaoItem = ({nome, missao, contato, backgroundColor}: Props) => {
+  const styles = getStyles(backgroundColor);
 
   return (
     <View style={styles.containerMissao}>
-      <View style={styles.containerItem}>
-        <Text style={styles.missao}>{nome}</Text>
-        <Text style={styles.missao}>{missao}</Text>
-        <Text style={styles.missao}>{contato}</Text>
-      </View>
+      <Text style={styles.missao}>{missao ? `${nome} (${missao})` : nome}</Text>
+      {contato && <Text style={styles.missao}>{contato}</Text>}
     </View>
   );
 };
 
-const getStyles = (backgroundColor: string, color: string) => {
+const getStyles = (backgroundColor: string) => {
   return StyleSheet.create({
     missao: {
-      color,
-      fontSize: wp('4%'),
+      color: SUBTEXT,
+      fontSize: 18,
       fontFamily: FONT_AVENIR_ROMAN,
       textAlign: 'center',
+      width: 250,
     },
     containerMissao: {
-      width: wp('80%'),
+      width: 302,
+      height: 64,
       marginBottom: hp('1%'),
-    },
-    containerItem: {
-      height: hp('8%'),
       backgroundColor,
       justifyContent: 'center',
+      alignItems: 'center',
       shadowOffset: {
         width: 0.2,
         height: 0.2,
