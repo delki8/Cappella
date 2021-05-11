5d265c0af27fab5aa855c18ec75709cc5e8c5fd1
Diogo Bastos <diogo.bastos@hmhco.com>
2021-05-11 14:22:53 -0400

    Ao vivo fallback

--- src/views/AoVivo/index.tsx
+++ src/views/AoVivo/index.tsx
@@ -1,35 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-const {useTracker} = require('@socialize/react-native-meteor');
 
 import {ContainerPage} from '../../components/ContainerPage';
 import {TITLE, FONT_AVENIR_ROMAN, WHITE, IRON} from '../../styles/styles';
 import {handlePress} from '../../utils/handlePress';
 import {LinkAovivo} from '../../../imports/api/linkAovivo';
+import {FALLBACK} from './data/AoVivo';
+import {ContainerServer} from '../../components/ContainerServer';
+import {handleIsConnected} from '../../utils/handleIsConnected';
+
+interface AoVivo {
+  url: string;
+}
 interface Props {
   titulo: string;
 }
 
 export const AoVivo = ({titulo}: Props) => {
   const styles = getStyles();
-  const AOVIVO = useTracker(() => LinkAovivo.find().fetch())[0];
+  const [isConnected, setIsConnected] = useState(false);
+
+  handleIsConnected().then((value) => {
+    setIsConnected(Boolean(value));
+  });
+
+  const aoVivoItem = (AOVIVO: AoVivo) => (
+    <TouchableOpacity
+      style={styles.containerItem}
+      onPress={() => handlePress(AOVIVO.url)}>
+      <Image
+        source={require('../../assets/images/video-icon.png')}
+        style={styles.imagem}
+        resizeMode="contain"
+      />
+    </TouchableOpacity>
+  );
 
   return (
     <ContainerPage titulo={titulo}>
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.containerItem}
-          onPress={() => handlePress(AOVIVO.url)}>
-          <Image
-            source={require('../../assets/images/video-icon.png')}
-            style={styles.imagem}
-            resizeMode="contain"
-          />
-        </TouchableOpacity>
+        {isConnected ? (
+          <ContainerServer collection={LinkAovivo}>
+            {(collection: AoVivo[]) => {
+              const AOVIVO = collection[0];
+
+              return <>{aoVivoItem(AOVIVO)}</>;
+            }}
+          </ContainerServer>
+        ) : (
+          <>{aoVivoItem(FALLBACK)}</>
+        )}
         <Text style={styles.notificacoes}>
           ative as notificações para ser sempre avisado quando estamos ao vivo!
         </Text>
