b319c25184b1bc884b3a492e1e362ed7047f1b1a
ruither.borba <delki8@gmail.com>
2021-08-16 15:45:41 -0300

    - adicionando integração com onesignal pra receber notificações no app

--- index.js
+++ index.js
@@ -5,5 +5,37 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import OneSignal from 'react-native-onesignal';
 
 AppRegistry.registerComponent(appName, () => App);
+
+//OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId('2c373247-c946-4129-ae38-c05accad3d67');
+//END OneSignal Init Code
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log('Prompt response:', response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(
+  (notificationReceivedEvent) => {
+    console.log(
+      'OneSignal: notification will show in foreground:',
+      notificationReceivedEvent,
+    );
+    let notification = notificationReceivedEvent.getNotification();
+    console.log('notification: ', notification);
+    const data = notification.additionalData;
+    console.log('additionalData: ', data);
+    // Complete with null means don't show a notification.
+    notificationReceivedEvent.complete(notification);
+  },
+);
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler((notification) => {
+  console.log('OneSignal: notification opened:', notification);
+});
