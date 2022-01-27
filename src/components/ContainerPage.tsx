7eeef1ad36a2a4897022a2fd2d87f2948ab7e3e9
ruither.borba <delki8@gmail.com>
2022-01-27 09:58:55 -0300

    - extraindo o componente de notificacao

--- src/components/ContainerPage.tsx
+++ src/components/ContainerPage.tsx
@@ -1,22 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faArrowLeft} from '@fortawesome/free-solid-svg-icons';
-import {faBell} from '@fortawesome/free-solid-svg-icons';
-import {faBellSlash} from '@fortawesome/free-solid-svg-icons';
-import AsyncStorage from '@react-native-community/async-storage';
 
 import {
   FONT_AVENIR_BLACK,
   IRON,
   ORANGEBUTTON,
+  SIZE_LARGE,
   SIZE_X_LARGE,
   SIZE_XXX_LARGE,
-  SIZE_LARGE,
 } from '../styles/styles';
+import {NotificationBell} from './NotificationBell';
 
 interface Props {
   titulo?: string;
@@ -25,31 +23,6 @@ interface Props {
 
 export const ContainerPage = ({titulo, children}: Props) => {
   const navigation = useNavigation();
-  const [optIn, setOptIn] = useState(false);
-  const notifyMeKey = 'notifyMe';
-
-  const storeData = async (value: string) => {
-    try {
-      await AsyncStorage.setItem(notifyMeKey, value);
-      console.log(`valor "${value}" armazenado com sucesso`);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  const getData = async () => {
-    try {
-      const value = await AsyncStorage.getItem(notifyMeKey);
-      if (value !== null) {
-        return value;
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-  useEffect(() => {
-    getData().then((value) => setOptIn(value === 'true'));
-  }, []);
 
   return (
     <>
@@ -64,16 +37,7 @@ export const ContainerPage = ({titulo, children}: Props) => {
             size={SIZE_LARGE}
           />
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            storeData((!optIn).toString()).then(() => setOptIn(!optIn));
-          }}>
-          <FontAwesomeIcon
-            icon={optIn ? faBell : faBellSlash}
-            color={optIn ? ORANGEBUTTON : IRON}
-            size={SIZE_LARGE}
-          />
-        </TouchableOpacity>
+        <NotificationBell />
       </View>
       {titulo && (
         <Text allowFontScaling={false} style={styles.titulo}>
---
+++ src/components/NotificationBell.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+import {TouchableOpacity} from 'react-native';
+
+import {IRON, ORANGEBUTTON, SIZE_LARGE} from '../styles/styles';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faBell, faBellSlash} from '@fortawesome/free-solid-svg-icons';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const NotificationBell = () => {
+  const [optIn, setOptIn] = useState(false);
+  const notifyMeKey = 'notifyMe';
+
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem(notifyMeKey, value);
+      console.log(`valor "${value}" armazenado com sucesso`);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem(notifyMeKey);
+      if (value !== null) {
+        return value;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    getData().then((value) => setOptIn(value === 'true'));
+  }, []);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        storeData((!optIn).toString()).then(() => setOptIn(!optIn));
+      }}>
+      <FontAwesomeIcon
+        icon={optIn ? faBell : faBellSlash}
+        color={optIn ? ORANGEBUTTON : IRON}
+        size={SIZE_LARGE}
+      />
+    </TouchableOpacity>
+  );
+};
